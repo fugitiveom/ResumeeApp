196ccb58c109f90a7cc17395b1fb4fd18596b604
fugitive <fugitiveom@gmail.com>
2023-07-08 17:17:52 +0400

    Release v.1

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from config import country, job_type, root_dir, job_portal
 company = input('Company: ')
 position = input('Position: ')
 
-workdir = f'{root_dir}{country}'
+workdir = root_dir + country
 
 modules.makedir(workdir, company)
 modules.copy_templates(workdir, company, job_type)
 modules.generate_email(workdir, company, position,job_portal)
 modules.generate_resume(workdir, company)
+modules.generate_cover_letter(workdir, company, position, job_portal)
+modules.remove_docx(workdir, company)
--- modules.py
+++ modules.py
@@ -1,16 +1,19 @@
 import os
 import shutil
 import glob
-from config import email_pref
+from config import email_regexp, resume_regexp, cover_letter_regexp
+import win32com.client as win32
+from datetime import date
 
 
 def makedir(workdir, company):
-    if not os.path.isdir(workdir + company):
-        os.mkdir(workdir + company)
+    company_dir = os.path.join(workdir, company)
+    if not os.path.isdir(company_dir):
+        os.makedirs(company_dir)
 
 
 def copy_templates(workdir, company, job_type):  # TODO убрать конкатенацию
-    templates_path = workdir + '/_templates'
+    templates_path = os.path.join(workdir + '/_templates')
     templates = glob.glob(templates_path + '/*.docx')
     templates += glob.glob(templates_path + '/*.txt')
     for template in templates:
@@ -21,7 +24,8 @@ def copy_templates(workdir, company, job_type):  # TODO убрать конка�
 
 
 def generate_email(workdir, company, position, job_portal):  # TODO Переписать на построчный вариант
-    source = glob.glob(workdir + '/' + company + '/' + email_pref)
+    source = glob.glob(workdir + '/' + company + '/' + email_regexp)
+    source[0] = source[0].replace('/', '\\')
     with open(source[0], 'r+') as f:
         data = f.read()
         data = data.replace('[position name]', position)
@@ -32,4 +36,46 @@ def generate_email(workdir, company, position, job_portal):  # TODO Перепи
 
 
 def generate_resume(workdir, company):
-    source = glob.glob(workdir + '/' + company + '/' + email_pref)
+    source = glob.glob(workdir + '/' + company + '/' + resume_regexp)
+    source[0] = source[0].replace('/', '\\')
+
+    word = win32.gencache.EnsureDispatch('Word.Application')
+    word.Visible = False
+
+    doc = word.Documents.Open(source[0])
+    doc.SaveAs(source[0].replace('docx', 'pdf').replace('tech', company), 17)
+    doc.Close()
+    word.Quit()
+
+
+def generate_cover_letter(workdir, company, position, job_portal):
+    source = glob.glob(workdir + '/' + company + '/' + cover_letter_regexp)
+    source[0] = source[0].replace('/', '\\')
+    word = win32.gencache.EnsureDispatch('Word.Application')
+    word.Visible = False
+
+    replacements = {
+        '[Position Title]' : position,
+        '[Company Name]' : company,
+        '[Platform/Source]' : job_portal,
+        '[Date]' : str(date.today())
+    }
+
+    doc = word.Documents.Open(source[0])
+
+    for find_text, replace_with in replacements.items():
+        for paragraph in doc.Paragraphs:
+            if find_text in paragraph.Range.Text:
+                paragraph.Range.HighlightColorIndex = 0
+                paragraph.Range.Text = paragraph.Range.Text.replace(find_text, replace_with)
+
+    doc.SaveAs(source[0].replace('docx', 'pdf').replace('tech', company), 17)
+    doc.Close()
+    word.Quit()
+
+
+def remove_docx(workdir, company):
+    docxs = glob.glob(workdir + '/' + company + '/' + '*.docx')
+    for docx in docxs:
+        os.remove(docx)
+
