444049b789be752c8008c60c028e2cc2d051c319
fugitive <fugitiveom@gmail.com>
2023-07-17 00:23:19 +0400

    удалил использование WinWord из кейса

--- modules/usecases.py
+++ modules/usecases.py
@@ -10,7 +10,6 @@ class WindowsWordCase:
         self.job_portal = job_portal
         self.prepare = Preparator(self.workdir, self.company, self.job_type)
         self.clear = GarbageRemover(self.workdir, self.company)
-        self.winword = WinWordAdapter()
         self.windocgen = WinDocsGenerator(self.workdir, self.company, self.job_type, self.position, self.job_portal)
 
     def make_documents(self):
