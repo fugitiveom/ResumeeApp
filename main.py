1a6d43feb4361cec5918c456a4e7268bec6e073b
fugitive <fugitiveom@gmail.com>
2023-07-16 18:40:40 +0400

    resume app v.2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import modules
+import os
 from config import country, job_type, root_dir, job_portal
 
 company = input('Company: ')
@@ -6,9 +7,6 @@ position = input('Position: ')
 
 workdir = root_dir + country
 
-modules.makedir(workdir, company)
-modules.copy_templates(workdir, company, job_type)
-modules.generate_email(workdir, company, position,job_portal)
-modules.generate_resume(workdir, company)
-modules.generate_cover_letter(workdir, company, position, job_portal)
-modules.remove_docx(workdir, company)
+if os.name == 'nt':
+    wingenerator = modules.GenerateDocsWin()
+    wingenerator.generate(workdir, company, job_type, position, job_portal)
