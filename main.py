a0e021a9967a1bf2b28ffbcb23a428e6fbba24f1
fugitive <fugitiveom@gmail.com>
2023-07-23 00:45:19 +0400

    usecase

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ position = input('Position: ')
 
 WORKDIR = ROOT_DIR + COUNTRY
 
+class UseCaseDataDTO:
+    def __init__(self, company, job_type, position, job_portal):
+        self.company = company
+        self.job_type = job_type
+        self.position = position
+        self.job_portal = job_portal
+
 if os.name == 'nt':
     winword_case = WindowsWordCase(WORKDIR, company, JOB_TYPE, position, JOB_PORTAL)
     winword_case.make_documents()
